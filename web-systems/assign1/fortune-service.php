aef0226fa3ed9b669e4d4ea6d022c1a602b08d92
Aaron Ingersoll <flex.box.hero@gmail.com>
2020-05-13 23:35:06 -0600

    starting web systems dev

---
+++ web-systems/assign1/fortune-service.php
@@ -0,0 +1,6 @@
+<?php
+$relationships_array[] = "You will have a large family";
+$relationships_array[] = "You will have a few close friends";
+$relationships_array[] = "You will have a smaller family, but many close friends";
+
+?>
